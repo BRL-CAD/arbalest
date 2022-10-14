26036faf00f0c9afea779150ed8cb800964f22ec
isaacy13 <iyastros@gmail.com>
2022-10-13 22:16:14 -0500

    changed dockable to reflect new mockup

--- include/VerificationValidationWidget.h
+++ include/VerificationValidationWidget.h
@@ -27,23 +27,52 @@ private:
     QString modelID;
     QString dbName;
     QString dbConnectionName;
-    
+
     // user interface
     QListWidget* testList;
     QTableWidget* resultTable;
 
-    // functions
+    // init functions
     void dbConnect();
     void dbInitTables();
     void dbPopulateTables();
     void dbInitDummyData();
     void setupUI();
-    QSqlQuery* dbExec(QString command, bool showErrorPopup = true);
-    void popup(QString message);
 
+    // database functions
+    QSqlQuery* dbExec(QString command, bool showErrorPopup = true);
     QSqlDatabase getDatabase() const {
         return QSqlDatabase::database(dbConnectionName);
     }
+
+    // UI functions
+    void popup(QString message);
+
+    // events
+    void resizeEvent(QResizeEvent* event) override;
+
+    // constant defaults
+    // TODO: default tests currently don't have suiteName... do we want this?
+    const std::vector<std::vector<QString>> defaultTests = {
+        {"No mis-matched dupliate IDs", "lc -m all"},
+        {"No nested regions", "search /all -type region -below -type region"},
+        {"No empty combos", "search /all -nnodes 0"},
+        {"No solids outside of regions", "search /all ! -below -type region -type shape"},
+        {"All BoTs are volume mode (should return nothing)", "search all -type bot ! -type volume"},
+        {"No BoTs are left hand orientation", "search all -type bot -param orient=lh"}, // TODO: this command can run faster if use unix
+        {"All regions have material & LOS", "search /all -type region ! -attr aircode ! -attr material_id"}, // TODO: same category of test, different test
+        {"All regions have material & LOS", "search /all -type region ! -attr aircode ! -attr los"}, // TODO: same category of test, different test
+        // TODO: missing "No errors when top level drawn"
+        {"No null region", "gqa -Ao -g4mm,4mm -t0.3mm all"},
+        // TODO: missing "BoTs are valid"
+        // TODO: missing "Air does not stick out"
+        {"Overlaps cleaned to 4mm gridsize with 0.3mm tolerance", "gqa -Ao -g32mm,4mm -t0.3mm all"},
+        {"Duplicate ID check", "lc -d all"},
+        {"No matrices", "search /all ! -matrix IDN"},
+        // TODO: missing "Title"
+        // TODO: missing "Ground plane at z=0"
+        {"No regions have aircodes (except actual air regions)", "search /all -type region -attr aircode"}
+    };
 };
 
 
--- src/gui/VerificationValidationWidget.cpp
+++ src/gui/VerificationValidationWidget.cpp
@@ -44,30 +44,8 @@ void VerificationValidationWidget::dbPopulateTables() {
     // note: this doesn't repopulate deleted tests, unless all tests deleted
     qResult = dbExec("SELECT id FROM Tests", !SHOW_ERROR_POPUP);
     if (!qResult->next()) {
-        // TODO: default tests currently don't have suiteName... do we want this?
-        const vector<pair<const QString, const QString>> defaultTests = {
-            {"No mis-matched dupliate IDs", "lc -m all"},
-            {"No nested regions", "search /all -type region -below -type region"},
-            {"No empty combos", "search /all -nnodes 0"},
-            {"No solids outside of regions", "search /all ! -below -type region -type shape"},
-            {"All BoTs are volume mode (should return nothing)", "search all -type bot ! -type volume"},
-            {"No BoTs are left hand orientation", "search all -type bot -param orient=lh"}, // TODO: this command can run faster if use unix
-            {"All regions have material & LOS", "search /all -type region ! -attr aircode ! -attr material_id"}, // TODO: same category of test, different test
-            {"All regions have material & LOS", "search /all -type region ! -attr aircode ! -attr los"}, // TODO: same category of test, different test
-            // TODO: missing "No errors when top level drawn"
-            {"No null region", "gqa -Ao -g4mm,4mm -t0.3mm all"},
-            // TODO: missing "BoTs are valid"
-            // TODO: missing "Air does not stick out"
-            {"Overlaps cleaned to 4mm gridsize with 0.3mm tolerance", "gqa -Ao -g32mm,4mm -t0.3mm all"},
-            {"Duplicate ID check", "lc -d all"},
-            {"No matrices", "search /all ! -matrix IDN"},
-            // TODO: missing "Title"
-            // TODO: missing "Ground plane at z=0"
-            {"No regions have aircodes (except actual air regions)", "search /all -type region -attr aircode"}
-        };
-
         for (int i = 0; i < defaultTests.size(); i++) {
-            cmd = "INSERT INTO Tests (testName, testCommand) VALUES ('" + defaultTests[i].first + "', '" + defaultTests[i].second + "')";
+            cmd = "INSERT INTO Tests (testName, testCommand) VALUES ('" + defaultTests[i][0] + "', '" + defaultTests[i][1] + "')";
             dbExec(cmd);
         }
     }
@@ -91,81 +69,37 @@ void VerificationValidationWidget::dbInitDummyData() {
 
 void VerificationValidationWidget::setupUI() {    
     QStringList columnLabels;
-    columnLabels << "Result Code" << "Problem Object/Region" << "Problem Description";
-    columnLabels << "Test ID" << "Test Suite" << "Test Name" << "Test Command";
-    columnLabels << "Terminal Output"; // TODO: move to popup?
+    columnLabels << "   " << "   " << "Test Name" << "Description" << "Object Path";
     resultTable->setColumnCount(columnLabels.size());
     resultTable->setHorizontalHeaderLabels(columnLabels);
-
-    // populate UI w/ db entries
-    QSqlQuery* qTestResults;
-    QString cmd = "SELECT resultCode, testID, terminalOutput FROM TestResults WHERE modelID=" + modelID;
-    qTestResults = dbExec(cmd);
-    int numRows = 0;
-    while (qTestResults && qTestResults->next()) {
-        QString resultCode = qTestResults->value(0).toString();
-        QString testID = qTestResults->value(1).toString();
-        QString terminalOutput = qTestResults->value(2).toString();
-
-        cmd = "SELECT testName, testCommand FROM Tests WHERE id=" + testID;
-        QSqlQuery* qResult = dbExec(cmd);
-        qResult->next();
-        
-        QString testName = qResult->value(0).toString();
-        QString testCommand = qResult->value(1).toString();
-
-        cmd = "SELECT suiteName FROM TestsSuites INNER JOIN TestsInSuite WHERE TestsSuites.id=TestsInSuite.testSuiteID AND TestsInSuite.testID=" + testID;
-        qResult = dbExec(cmd);
-        QString suiteName = (qResult->next()) ? qResult->value(0).toString() : "";
-
-        cmd = "SELECT objectName, issueDescription FROM ObjectIssue INNER JOIN Issues WHERE Issues.testID=" + testID;
-        qResult = dbExec(cmd);
-        QString problemObjectName = "";
-        QString issueDescription = "";
-        if (qResult->next()) {
-            problemObjectName = qResult->value(0).toString();
-            issueDescription = qResult->value(1).toString();
-        }
-
-        resultTable->setRowCount(numRows + 1);
-        // put everything onto view
-        QTableWidgetItem* item0 = new QTableWidgetItem(tr("%1").arg(resultCode));
-        resultTable->setItem(numRows, 0, item0);
-        QTableWidgetItem* item1 = new QTableWidgetItem(tr("%1").arg(problemObjectName));
-        resultTable->setItem(numRows, 1, item1);
-        QTableWidgetItem* item2 = new QTableWidgetItem(tr("%1").arg(issueDescription));
-        resultTable->setItem(numRows, 2, item2);
-        QTableWidgetItem* item3 = new QTableWidgetItem(tr("%1").arg(testID));
-        resultTable->setItem(numRows, 3, item3);
-        QTableWidgetItem* item4 = new QTableWidgetItem(tr("%1").arg(suiteName));
-        resultTable->setItem(numRows, 4, item4);
-        QTableWidgetItem* item5 = new QTableWidgetItem(tr("%1").arg(testName));
-        resultTable->setItem(numRows, 5, item5);
-        QTableWidgetItem* item6 = new QTableWidgetItem(tr("%1").arg(testCommand));
-        resultTable->setItem(numRows, 6, item6);
-        QTableWidgetItem* item7 = new QTableWidgetItem(tr("%1").arg(terminalOutput));
-        resultTable->setItem(numRows, 7, item7);
-        
-        ++numRows;
-    }
-
-    // TODO: make custom collapsible component (with check box)... consider reusing CollapsibleWidget... if not: https://stackoverflow.com/questions/11077793/is-there-a-standard-component-for-collapsible-panel-in-qt
-    QStringList tests;
-    tests << "test 1" << "test 2" << "test 3" << "test 4";
-    testList->addItems(tests);
-
-    QListWidgetItem* item = 0;
-    for (int i = 0; i < testList->count(); i++) {
-        item = testList->item(i);
-        item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
-        item->setCheckState(Qt::Unchecked);
-    }
-
-    addWidget(testList);
+    resultTable->verticalHeader()->setVisible(false);
+    resultTable->horizontalHeader()->setStretchLastSection(true);
+    resultTable->horizontalHeader()->setDefaultAlignment(Qt::AlignLeft);
     addWidget(resultTable);
 
-    getBoxLayout()->setStretchFactor(testList, 1);
-    getBoxLayout()->setStretchFactor(resultTable, 3);
+    // TODO: make custom collapsible component (with check box)... consider reusing CollapsibleWidget... if not: https://stackoverflow.com/questions/11077793/is-there-a-standard-component-for-collapsible-panel-in-qt
+    // QStringList tests;
+    // tests << "test 1" << "test 2" << "test 3" << "test 4";
+    // testList->addItems(tests);
+
+    // QListWidgetItem* item = 0;
+    // for (int i = 0; i < testList->count(); i++) {
+    //     item = testList->item(i);
+    //     item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
+    //     item->setCheckState(Qt::Unchecked);
+    // }
+
+    // addWidget(testList);
+}
+
+void VerificationValidationWidget::resizeEvent(QResizeEvent* event) {
+    resultTable->setColumnWidth(0, this->width() * 0.025);
+    resultTable->setColumnWidth(1, this->width() * 0.025);
+    resultTable->setColumnWidth(2, this->width() * 0.10);
+    resultTable->setColumnWidth(3, this->width() * 0.60);
+    resultTable->setColumnWidth(4, this->width() * 0.25);
+
+    QHBoxWidget::resizeEvent(event);
 }
 
 VerificationValidationWidget::~VerificationValidationWidget() {
