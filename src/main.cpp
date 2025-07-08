8a6bee5ad4d7a847e78777771917731ef6ba500e
Lorenzo Pegorari <lorenzo.pegorari2002@gmail.com>
2025-07-08 23:10:10 +0200

    Removed useless references to old or user-specific paths

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,10 @@
-//#define ARB_DEBUG
-
 #include <QApplication>
 #include <QOpenGLWidget>
 #include "MainWindow.h"
 
 int main(int argc, char*argv[]) {
-
-#ifdef ARB_DEBUG
-    if (argc < 2) {
-        argv = new char* [2]{ argv[0], "C:/Users/Sadeep/Desktop/moss.g" };
-        argc = 2;
-    }
-#endif
-
-
     QApplication app(argc,argv);
     MainWindow mainWindow;
     mainWindow.showMaximized();
     return app.exec();
 }
-
-/*
-INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    PSTR lpCmdLine, INT nCmdShow)
-{
-
-// also add to cmake: add_executable(arbelst WIN32
-    int argc = 1;
-    char* argv[1]{ "" };
-
-    QApplication app(argc, argv);
-    MainWindow mainWindow;
-    mainWindow.show();
-    QApplication::exec();
-    return 0;
-}
-*/
