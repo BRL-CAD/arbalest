38d4cd1b6b389b959ad09f8593916fc88ab46eaf
Lorenzo Pegorari <lorenzo.pegorari2002@gmail.com>
2025-07-30 23:32:39 +0200

    Added copyright and license statement

--- src/gui/Console.cpp
+++ src/gui/Console.cpp
@@ -1,3 +1,25 @@
+/*                        C O N S O L E . C P P
+ * BRL-CAD
+ *
+ * Copyright (c) 2025 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file Console.cpp */
+
+
 #include <QAbstractItemView>
 #include <QApplication>
 #include <QClipboard>
